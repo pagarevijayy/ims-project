765264420c245b2f8bafbba53d6f2e4cc4752a8b
Vijay Pagare <pagarevj97@gmail.com>
2022-03-10 17:44:56 +0530

    PQ txn view created

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import ProductMaster from "../components/raw-materials/product-master-view";
 import PQView from "../components/raw-materials/pq-view";
 import WIPView from "../components/raw-materials/wip-view";
 import FinishedView from "../components/raw-materials/finished-view";
+import PQTnxView from "../components/raw-materials/pq-txn-view";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       </Head>
       <div className="h-screen w-screen d-flex items-center justify-center text-center p-16">
         <h1 className="text-3xl font-bold">IMS Frontend</h1>
-        <FinishedView />
+        <PQTnxView />
       </div>
     </>
   );
